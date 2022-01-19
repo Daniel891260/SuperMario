8f65c45c5d1bb10f66f12581459482305ed15876
Daniel891260 <77424289+Daniel891260@users.noreply.github.com>
2022-01-18 16:58:50 -0800

    Add files via upload

---
+++ public/js/input.js
@@ -0,0 +1,31 @@
+import Keyboard from './KeyboardState.js';
+import InputRouter from './InputRouter.js';
+
+export function setupKeyboard(window) {
+    const input = new Keyboard();
+    const router = new InputRouter();
+
+    input.listenTo(window);
+
+    input.addMapping('KeyP', keyState => {
+        if (keyState) {
+            router.route(entity => entity.jump.start());
+        } else {
+            router.route(entity => entity.jump.cancel());
+        }
+    });
+
+    input.addMapping('KeyO', keyState => {
+        router.route(entity => entity.turbo(keyState));
+    });
+
+    input.addMapping('KeyD', keyState => {
+        router.route(entity => entity.go.dir += keyState ? 1 : -1);
+    });
+
+    input.addMapping('KeyA', keyState => {
+        router.route(entity => entity.go.dir += keyState ? -1 : 1);
+    });
+
+    return router;
+}
